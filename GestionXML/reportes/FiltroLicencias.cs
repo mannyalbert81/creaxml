7990b961292db75699dd2b46b04dd18385c0b366
STEVEN TITUAÑA <steven642010@hotmail.com>
2017-05-04 12:13:54 -0500

    t

--- GestionXML/reportes/FiltroLicencias.cs
+++ GestionXML/reportes/FiltroLicencias.cs
@@ -20,9 +20,15 @@ namespace GestionXML.reportes
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int _id_Licencias = Convert.ToInt32(cmb_licencias.SelectedValue.ToString());
+            string _descripcion_licencias = AccesoLogica.cifrar(cmb_licencias.Text);
+            int _id_Licencias = 0;
 
-
+            DataTable dtLicencias = AccesoLogica.Select("*", "licencias", "descripcion_licencias = '"+ _descripcion_licencias + "'");
+            foreach (DataRow renglon in dtLicencias.Rows)
+            {
+                _id_Licencias = Convert.ToInt32(renglon["id_licencias"].ToString());
+            }
+            
             reportes.contLicencias frm = new reportes.contLicencias();
             frm._id_Licencias = _id_Licencias;
             frm.Show();
@@ -30,22 +36,28 @@ namespace GestionXML.reportes
 
         private void FiltroLicencias_Load(object sender, EventArgs e)
         {
+
+            llenar_combomanual();
+
+        }
+        
+
+        private void llenar_combomanual()
+        {
             int _id_licencias = 0;
             string _descripcion_licencias = "";
-
-
 
             DataTable dtLicencias = AccesoLogica.Select("*", "licencias", "licencias.id_licencias > 0");
             foreach (DataRow renglon in dtLicencias.Rows)
             {
+             
                 _id_licencias = Convert.ToInt32(renglon["id_licencias"].ToString());
                 _descripcion_licencias = AccesoLogica.descifrar(Convert.ToString(renglon["descripcion_licencias"].ToString()));
 
-                clases.Funciones.CargarCombo(cmb_licencias, "'" + _id_licencias + "'", "'" + _descripcion_licencias + "'", "licencias");
-
+                cmb_licencias.Items.Add(_descripcion_licencias);
 
             }
-
+ 
         }
     }
 }
